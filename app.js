642dc1a7dbe3e1680e7782721b99f251fd1da841
Dhawal Tiwari <98008402+tiwariDhawal@users.noreply.github.com>
2022-05-25 16:16:05 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.send("Hello");
+});
+
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
+});
